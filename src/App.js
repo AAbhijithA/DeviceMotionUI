622b3a59d7afd655aa4902d4397136bdfaff04ea
AAbhijithA <abhijithajith2021@gmail.com>
2025-02-18 22:22:25 +0530

    Map fixes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ import {
   TileLayer,
   useMap,
   Marker,
-} from 'react-leaflet'
+} from 'react-leaflet';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 ChartJS.register(
   CategoryScale,
